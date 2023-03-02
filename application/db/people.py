508995e235506bcfbab73c4bc0b40f84bba142f3
Aleksandr Rossikhin <ross1990@mail.ru>
2023-03-02 23:39:52 +0300

    before review

---
+++ application/db/people.py
@@ -0,0 +1,5 @@
+def get_employees():
+    print(f"The staff includes people")
+
+if __name__ == '__main__':
+    get_employees()
--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+from datetime import datetime
+import yadisk
+from application.salary import calculate_salary
+from application.db.people import get_employees
 
+
+if __name__ == '__main__':
+    print(datetime.now())
+    calculate_salary()
+    get_employees()
